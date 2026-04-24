2efd1301dde568505eb9f5db036a21c0912a6b40
sangita avghad <sangitaavghad1987@gmail.com>
2026-04-24 13:44:27 +0530

    Sangita: Adds functionlity of add items and countig totalamount each item

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MealItem.css";
 
-function MealItem(props){
+function MealItem(props) {
+  const [count, setCount] = useState(0);
 
-    return(
-        <div className="meal-item-container">
+  const handleOnClick = () => {
+    setCount((prev) => prev + 1);
+  };
 
-            <h5>{props.meal.name}</h5>
-            <p className="meal-description">{props.meal.description}</p>
-            <p className="meal-price">${props.meal.price}</p>
-    
-        </div>
-    )
+  const totalAmount = count * props.meal.price;
+
+  return (
+    <div className="meal-item-container">
+      <h5>{props.meal.name}</h5>
+
+      <div className="description-name-container">
+        <p className="meal-description">{props.meal.description}</p>
+
+        <p style={{ color: "black" }}>
+          Amount{" "}
+          <span className="badge">{count}</span>
+        </p>
+      </div>
+
+      <div style={{ position: "relative" }}>
+        <p className="meal-price">
+          ${props.meal.price}
+          {" "}
+          <span className="btn-container">
+            <button className="add-btn" onClick={handleOnClick}>
+              + Add
+            </button>
+          </span>
+        </p>
+      </div>
+
+      {/* ✅ Total Price */}
+      {count > 0 && (
+        <p className="total-amount">
+          Total: ${totalAmount}
+        </p>
+      )}
+    </div>
+  );
 }
 
 export default MealItem;
