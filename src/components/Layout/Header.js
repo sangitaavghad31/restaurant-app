ca99b3959b9d2dff40ccd97cb8e5c5079eb02708
sangita avghad <sangitaavghad1987@gmail.com>
2026-04-25 15:43:07 +0530

    Sangita: adds functionlity to close button of cart compoenent

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -5,11 +5,13 @@ import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
+    console.log("Hi");
+    console.log(props)
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food!' />
